ede216aee88985f1cac31c96759e17aec6ecafba
anmol420 <anmolanand2k15@gmail.com>
2024-01-30 01:16:42 +0530

    Added Changes

---
+++ day_06/README.md
@@ -0,0 +1,9 @@
+# Functions In Rust - Day 6
+
+This is the sixth day you have to Take this challenge and start your #100daysofrust journey.
+
+## Task
+
+- Fork this repository
+- Now Using The [Resource](https://doc.rust-lang.org/book/ch03-03-how-functions-work.html) - Learn About `functions`.
+- Now Create a file name - `functions.rs` and add a `main` function in it.
--- day_06/function.rs
+++ day_06/function.rs
@@ -11,13 +11,16 @@ fn goodbye1(message: &str) {
 fn divide_by_3(num: u32) -> u32 {
     num / 3
 }
+
 fn main() {
     println!("Hello! my rusty fellows");
     goodbye();
+
     let formal = "formal: goodbye!";
     let informal = "informal: see you later";
     goodbye1(formal);
     goodbye1(informal);
+    
     let num: u32 = 69;
     println!("{} divided by 3 = {}", num, divide_by_3(num));
 }
