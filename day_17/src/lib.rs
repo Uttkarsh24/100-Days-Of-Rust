731ba9bac0f3fe442b03e9bf7c964389ee1e9a78
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-02-11 00:09:27 +0530

    ADDED code for day 17

---
+++ day_17/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code,unused_variables)]
+
+mod database;
+mod auth_utils;
+
+use auth_utils::models::Credentials;
+use database::Status;
+
+pub fn authenticate(creds: Credentials) {
+    if let Status::Connected=database::connect_to_database() {
+        auth_utils::login(creds);
+    }
+
+}
+
